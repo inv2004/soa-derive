c8502aa986830f24ffd54afaafbe906a0162efb3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2017-12-21 17:08:23 +0000

    Deploy lumol-org/soa-derive to github.com/lumol-org/soa-derive.git:gh-pages

---
+++ implementors/core/clone/trait.Clone.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["soa_derive_example"] = ["impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"soa_derive_example/struct.ParticleRef.html\" title=\"struct soa_derive_example::ParticleRef\">ParticleRef</a>&lt;'a&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/marker/trait.Copy.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["soa_derive_example"] = ["impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"soa_derive_example/struct.ParticleRef.html\" title=\"struct soa_derive_example::ParticleRef\">ParticleRef</a>&lt;'a&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
